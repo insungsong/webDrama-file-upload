f33677d06ad0c182c06dd16feff4582526a82588
insungsong <song22861@naver.com>
2020-10-14 11:36:17 +0900

    error

--- src/server.js
+++ src/server.js
@@ -414,7 +414,7 @@ passport.use(
 app.get("/login/naver", passport.authenticate("naver"));
 app.use(passport.initialize());
 
-app.post("/login/naver/callback", function (req, res, next) {
+app.get("/login/naver/callback", function (req, res, next) {
 console.log("1");  
 passport.authenticate("naver", function (err, user) {
   console.log("2");
