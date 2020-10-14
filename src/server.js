39417d1eab472113d44f3002e5e5ae508267e3a8
insungsong <song22861@naver.com>
2020-10-14 16:24:23 +0900

    cookie error

--- src/server.js
+++ src/server.js
@@ -6,8 +6,10 @@ import cors from "cors";
 import path from "path";
 import dotenv from "dotenv";
 import Cookie from "js-cookie";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cookieParser());
 
 // http://localhost:3000(frontend에서 localhost:5000이 들어오게 하는것을 허락하는 코드)
 app.use(cors());
