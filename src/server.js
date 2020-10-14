537043be5d4e4cf19a591d84263aa5c6f14ea17d
insungsong <song22861@naver.com>
2020-10-14 16:19:39 +0900

    cookie error

--- src/server.js
+++ src/server.js
@@ -423,7 +423,7 @@ passport.authenticate("naver", function (err, user) {
     req.logIn(user, function (err) {
       const current_NaverUser = user._json.email;
 
-      res.cookie("current_NaverUser", current_NaverUser, {secure:false,httpOnly:false});
+      res.cookie("current_NaverUser", current_NaverUser, {maxAge:20000,secure:false,httpOnly:false});
       return res.redirect(`https://weberyday.netlify.app/#/${current_NaverUser}`);
     });
   })(req, res);
