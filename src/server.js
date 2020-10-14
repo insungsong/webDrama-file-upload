9462a7ed2c8959cc98f30ad3f2de87ce69f6623b
insungsong <song22861@naver.com>
2020-10-14 13:13:00 +0900

    error

--- src/server.js
+++ src/server.js
@@ -6,8 +6,11 @@ import cors from "cors";
 import path from "path";
 import dotenv from "dotenv";
 import Cookie from "js-cookie";
+import sslRedirect from "heroku-ssl-redirect";
 
 const app = express();
+
+app.use(sslRedirect())
 
 // http://localhost:3000(frontend에서 localhost:5000이 들어오게 하는것을 허락하는 코드)
 app.use(cors());
