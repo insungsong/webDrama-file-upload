7be092637488bd0e9ed68c6f2eef3eeb76cdb846
insungsong <song22861@naver.com>
2020-10-14 16:55:58 +0900

    error

--- src/server.js
+++ src/server.js
@@ -427,7 +427,7 @@ passport.authenticate("naver", function (err, user) {
     req.logIn(user, function (err) {
       const current_NaverUser = user._json.email;
 
-      res.cookie("current_NaverUser", current_NaverUser, {maxAge:20000,secure:false,httpOnly:false});
+      res.cookie("current_NaverUser", current_NaverUser, {secure:true});
       return res.redirect(`https://weberyday.netlify.app/#/`);
     });
   })(req, res);
